a1f5970d141370a2c729f6fb3f185b3b85bec715
Brooks Gunn <brooks.a.gunn@gmail.com>
2023-12-30 22:59:12 -0500

    Add show/hide toggle on mobile menu

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
+let menu = document.querySelector(".menu");
+let nav = document.querySelector("nav");
 
+menu.addEventListener("click", toggleMenu);
+
+function toggleMenu() {
+  nav.classList.toggle("hide");
+  console.log(nav.classList);
+}
